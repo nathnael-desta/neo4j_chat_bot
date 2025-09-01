35afc1b8bac95f7fbb0fb9b1669ddc4b7eda8f18
Nathnael Desta <destanathnaelataro@gmail.com>
2025-09-01 09:53:53 +0300

    feat: add thinking animation

--- frontend/src/components/custom/message.tsx
+++ frontend/src/components/custom/message.tsx
@@ -46,25 +46,25 @@ export function PreviewMessage({ message }: { message: message }) {
 }
 
 export function ThinkingMessage() {
-  const role = 'assistant';
-
   return (
-    <motion.div
-      className="w-full mx-auto max-w-3xl px-4 group/message "
-      initial={{ y: 5, opacity: 0 }}
-      animate={{ y: 0, opacity: 1, transition: { delay: 0.2 } }}
-      data-role={role}
-    >
-      <div
-        className={cx(
-          'flex gap-4 group-data-[role=user]/message:px-3 w-full group-data-[role=user]/message:w-fit group-data-[role=user]/message:ml-auto group-data-[role=user]/message:max-w-2xl group-data-[role=user]/message:py-2 rounded-xl',
-          'group-data-[role=user]/message:bg-muted'
-        )}
-      >
-        <div className="size-8 flex items-center rounded-full justify-center ring-1 shrink-0 ring-border">
-          <SparklesIcon size={14} />
+    <div className="flex flex-col group md:items-center">
+      <div className="flex gap-4 p-4 md:max-w-3xl md:w-full">
+        <div className="w-8 h-8 rounded-full bg-secondary flex items-center justify-center text-primary shrink-0">
+          <Bot size={20} />
+        </div>
+        <div className="flex flex-col min-w-0 w-full">
+          <div className="font-semibold">
+            Assistant
+          </div>
+          <div className="min-w-0 h-8 flex items-center">
+            <div className="bouncing-loader">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
-};
+}
